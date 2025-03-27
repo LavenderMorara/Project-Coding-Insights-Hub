06566249f098f76ef6ceb9e0852f14b8e9d63045
LavenderMorara <chiyamorara@gmail.com>
2025-03-27 13:44:44 +0300

    displayed the server content

--- js/index.js
+++ js/index.js
@@ -1,7 +1,6 @@
 const baseUrl="http://localhost:3000"
 const resources="Projects"
 
-const analysisDisplay=document.querySelector('#analysis')
 let selectedProject;
 
 document.addEventListener('DOMContentLoaded', fetchAnalysis)
@@ -9,7 +8,23 @@ document.addEventListener('DOMContentLoaded', fetchAnalysis)
 function fetchAnalysis(){
     fetch(`${baseUrl}/${resources}`)
     .then(res=>res.json())
-    .then(data=>console.log(data))
+    .then(data=>displayImages(data))
     .catch(err=>console.log(err))
   
+}
+
+function displayImages(resources){
+    resources.forEach(project => {
+        selectedProject=project
+        const analysisDisplay=document.querySelector('#analysis')
+        let img=document.createElement('img');
+        let h3=document.createElement('h3')
+        h3.textContent=selectedProject.title
+        img.src=selectedProject.image
+        img.alt=selectedProject.title
+        img.style.height="270px"
+        img.style.width="240px"
+        analysisDisplay.append(img) 
+        analysisDisplay.appendChild(h3)    
+    });
 }
